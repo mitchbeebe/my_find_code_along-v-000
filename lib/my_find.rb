b9403016c58a23765051dcd492049edbf2b17858
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-09-04 01:38:32 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,9 +3,7 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
-    return if yield(collection[i])
-      return yield(collection[i])
-    end
+    return collection[i] if yield(collection[i])
     i += 1
   end
 end
